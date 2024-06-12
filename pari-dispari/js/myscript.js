eaeb187a0b1e14e744e4195937413895b74f8015
Samgrass <samuelegrassetti01@gmail.com>
2024-06-12 16:51:53 +0200

    aggiunto prompt per chiedere una parola all'utente, inizio funzione per sapere se è palindroma: divisione in caratteri, inversione caratteri, ricollegamento dei caratteri

--- pari-dispari/js/myscript.js
+++ pari-dispari/js/myscript.js
@@ -1,1 +1,21 @@
+// chiedere all'utente una parola
+const inputUtente = prompt("scrivi una parola, ti dirò se è palindroma");
+// creare una funzione con la parola dell'utente come argomento per sapere se è palindroma 
+function palindroma(parola) {
+    // prendere la parola e trasformarla in un array con ogni carattere come elemento
+    let parolaDivisa = parola.split("");
 
+    // invertire l'ordine dell'array
+    let parolaInvertita = parolaDivisa.reverse();
+
+    // far tornare l'array una stringa unica
+    let parolaCollegata = parolaInvertita.join("");
+
+    
+
+    // se la stringa inserita dall'utente è uguale a quella invertita è palindroma
+    
+    // altrimenti no 
+}
+
+    
