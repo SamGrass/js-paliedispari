ef026c78a70a7e94919b44f7683c5dbfa45ca678
Samgrass <samuelegrassetti01@gmail.com>
2024-06-12 17:29:08 +0200

    aggiungo prompt per chiedere un numero da 1 a 5 e se il totale è pari o dispari, aggiunto numero random da 1 a 5 per CPU

--- palindromo/js/myscript.js
+++ palindromo/js/myscript.js
@@ -1,13 +1,16 @@
 // utente e computer inseriscono due numeri che si sommano, in base al risultato e la scelta pari o dispari fatta dall'utente, si decreta il vincitore
 
 // utente inserisce pari o dispari
-
+const parolaUtente = prompt("scrivi se la somma sarà pari o dispari");
 // utente inserisce un numero da 1 a 5
-
+const numeroUtente = parseInt(prompt("digita un numero da 1 a 5"));
 // genero un numero random per il computer
+const numeroCPU = Math.floor(Math.random() * 5) + 1;
+console.log(numeroCPU);
 
 // sommo i due numeri 
 
-// controllo se la somma è pari o dispari 
+
+// controllo se la somma è pari o dispari
 
 // dichiaro il vincitore
