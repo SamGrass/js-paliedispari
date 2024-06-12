cbc3e2631e6ed1cb23f7146f21b5e466ead17c69
Samgrass <samuelegrassetti01@gmail.com>
2024-06-12 17:10:29 +0200

    aggiunti commenti js

---
+++ palindromo/js/myscript.js
@@ -0,0 +1,13 @@
+// utente e computer inseriscono due numeri che si sommano, in base al risultato e la scelta pari o dispari fatta dall'utente, si decreta il vincitore
+
+// utente inserisce pari o dispari
+
+// utente inserisce un numero da 1 a 5
+
+// genero un numero random per il computer
+
+// sommo i due numeri 
+
+// controllo se la somma è pari o dispari 
+
+// dichiaro il vincitore
