9c8f15346c15316ce41b22f06b3462c71c023640
Samgrass <samuelegrassetti01@gmail.com>
2024-06-12 17:44:15 +0200

    centralizzo la funzione, dichiaro se l'utente ha vinto o perso

--- palindromo/js/myscript.js
+++ palindromo/js/myscript.js
@@ -4,17 +4,36 @@
 const parolaUtente = prompt("scrivi se la somma sarà pari o dispari");
 // utente inserisce un numero da 1 a 5
 const numeroUtente = parseInt(prompt("digita un numero da 1 a 5"));
+console.log(numeroUtente);
 // genero un numero random per il computer
 const numeroCPU = Math.floor(Math.random() * 5) + 1;
 console.log(numeroCPU);
 
 // sommo i due numeri 
+// // function somma(num1, num2) {
+// //     return num1 + num2; 
+// // }
+
+// // console.log(somma(numeroUtente, numeroCPU));
+
+// controllo se la somma è pari o dispari
+// // function tipoNumero(totale) {
+// //     return totale % 2 === 0 ? "pari" : "dispari";
+// // }
+
+// // console.log(tipoNumero(somma(numeroUtente, numeroCPU)));
+
+
+// calcolo somma e se il numero è pari o dispari
 function somma(num1, num2) {
-    return num1 + num2; 
+    let totale = num1 + num2; 
+    return totale % 2 === 0 ? "pari" : "dispari";
 }
 
 console.log(somma(numeroUtente, numeroCPU));
 
-// controllo se la somma è pari o dispari
-
 // dichiaro il vincitore
+
+let risultato = parolaUtente === somma(numeroUtente, numeroCPU) ? "Hai vinto" : "Hai perso";
+
+console.log(risultato);
