d9b30837fd2995b648ca8a574e097983e0745d2a
Samgrass <samuelegrassetti01@gmail.com>
2024-06-12 17:04:52 +0200

    aggiunta funzione InputUser FINE ESERCIZIO PALINDROMO

--- pari-dispari/js/myscript.js
+++ pari-dispari/js/myscript.js
@@ -3,19 +3,22 @@ const inputUtente = prompt("scrivi una parola, ti dirò se è palindroma");
 // creare una funzione con la parola dell'utente come argomento per sapere se è palindroma 
 function palindroma(parola) {
     // prendere la parola e trasformarla in un array con ogni carattere come elemento
-    let parolaDivisa = parola.split("");
+    let parolaDivisa = parola.split('');
+    console.log(parolaDivisa);
 
     // invertire l'ordine dell'array
     let parolaInvertita = parolaDivisa.reverse();
+    console.log(parolaInvertita);
 
     // far tornare l'array una stringa unica
-    let parolaCollegata = parolaInvertita.join("");
+    let parolaCollegata = parolaInvertita.join('');
+    console.log(parolaCollegata);
 
     // let parolaInvertita = parola.split("").reverse().join("");
-
-    // se la stringa inserita dall'utente è uguale a quella invertita è palindroma
-    return parola === parolaCollegata ? "la parola è palindroma" : "la parola non è palindroma"
-    // altrimenti no 
+    // console.log(parolaInvertita);
+    
+    // se la stringa inserita dall'utente è uguale a quella invertita è palindroma oppure no
+    return parola === parolaCollegata ? console.log("la parola è palindroma") : console.log("la parola non è palindroma");
 }
 
-    
+palindroma(inputUtente);  
